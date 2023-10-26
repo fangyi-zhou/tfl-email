2ffbffca3b17b5587a3ac3b7fff48a5a4181c4ca
Fangyi Zhou <me@fangyi.io>
2023-10-26 23:12:38 +0100

    Add telegram integration

--- hello_world/app.py
+++ hello_world/app.py
@@ -3,12 +3,18 @@ import json
 import os
 
 import boto3
+import requests
 
 import llm
 
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 SECRET_NAME = os.environ.get("GCP_SECRET_NAME")
 REGION_NAME = "eu-west-1"
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+TELEGRAM_API_ENDPOINT = (
+    "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage"
+)
 
 
 def load_gcp_credentials():
@@ -24,6 +30,7 @@ def load_gcp_credentials():
 def lambda_handler(event, context):
     load_gcp_credentials()
     print("Loaded GCP Credentials successfully")
+
     s3 = boto3.resource("s3")
     message_id = event["Records"][0]["ses"]["mail"]["messageId"]
     email_object = s3.Object(
@@ -33,6 +40,7 @@ def lambda_handler(event, context):
     resp = email_object.get()
     data = resp["Body"].read()
     print(f"Loaded email message ID {message_id}")
+
     msg = email.message_from_bytes(data)
     texts = [
         part.get_payload()
@@ -41,6 +49,16 @@ def lambda_handler(event, context):
     ]
     summary = llm.produce_summary("\n".join(texts))
     print(f"Got summary from LLM: {summary}")
+
+    r = requests.post(
+        TELEGRAM_API_ENDPOINT,
+        data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": summary,
+            "parse_mode": "Markdown",
+        },
+    )
+    print(f"Got response from Telegram {r.json()}")
 
     return {
         "statusCode": 200,
