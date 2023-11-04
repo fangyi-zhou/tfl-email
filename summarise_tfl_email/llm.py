8a793c33f032a9e18c01234a253edcb608b0583b
Fangyi Zhou <me@fangyi.io>
2023-11-04 16:27:16 +0000

    Minor tweaks to LLM prompt and parameters

--- summarise_tfl_email/llm.py
+++ summarise_tfl_email/llm.py
@@ -6,14 +6,12 @@ from vertexai.language_models import TextGenerationModel
 PROJECT = os.environ.get("GCP_PROJECT")
 LOCATION = os.environ.get("GCP_LOCATION", "us-central1")
 PROMPT = """
-Use the provided weekend travel information. \
-Please provide a summary of disruptions and events happening this weekend.
-
-When describing disruptions, please include station names, line names, and \
-dates, for example:
-On [DATE], no service on [LINE] between [STATION] and [STATION].
-
-When describing events, please REMOVE "Large crowds are expected" or "Increased traffic is expected".
+Given a summary of disruptions and events happening this weekend using the \
+provided weekend travel information.
+For disruptions, include station names, line names, and dates.
+Remember to include disruptions of all modes of transport: Tube, London
+Overground, DLR, Elizabeth Line, Trams and Trains.
+For events, remove sentences about increased traffic or large crowds.
 
 Weekend travel information: %s
 
@@ -34,9 +32,9 @@ def produce_summary(content: str) -> str:
     parameters = {
         "candidate_count": 1,
         "max_output_tokens": 1024,
-        "temperature": 0.1,
-        "top_p": 0.25,
-        "top_k": 30,
+        "temperature": 0.05,
+        "top_p": 0.15,
+        "top_k": 20,
     }
     model = TextGenerationModel.from_pretrained("text-bison")
     text_input = PROMPT % content
